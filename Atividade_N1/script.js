c91e275e961cfb0b91f1f9b9d02c8113c68e7ed9
Yasmin-Sousa <148486314+Yasmin-Sousa@users.noreply.github.com>
2025-07-24 18:01:07 -0300

    Update script.js

--- Atividade_N1/script.js
+++ Atividade_N1/script.js
@@ -1,3 +1,15 @@
-function botaoRespostas() {
-    
+//const radio1 =
+//document.querySelector("input[name='pergunta1']:checked").value
+
+//console.log(radio1)
+let mensagemSucesso = "Acertou!"
+let P5b = document.getElementById("p5")
+//const Perg5 = document.getElementById("p5")
+const checkbox = document.getElementById("p5b");
+const isChecked = checkbox.checked;
+if (isChecked){
+    console.log(acertou)
+    P5b.textContent = mensagemSucesso
+} else {
+    console.log(errou)
 }
