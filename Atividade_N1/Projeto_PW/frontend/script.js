e5ca42a0f53fa4f2210cc0b58bdf723f8f247c00
Yasmin-Sousa <148486314+Yasmin-Sousa@users.noreply.github.com>
2025-08-04 10:08:19 -0300

    Add files via upload

--- Atividade_N1/Projeto_PW/frontend/script.js
+++ Atividade_N1/Projeto_PW/frontend/script.js
@@ -43,20 +43,55 @@ function verificarQuiz() {
     // Gabarito com as respostas corretas
     const respostasCorretas = {
       pergunta1: "Pérola",
-      pergunta4: "Canhão de Luz Laser",
-      pergunta2: "Terraformar planetas",
-      pergunta3: "Diamante Azul"
+      pergunta2: "Canhão de Luz Laser",
+      pergunta3: "Diamante Azul",
+      pergunta4: "Terraformar planetas",
+      pergunta6: "Barriga",
+      pergunta7: "2013-11-04",
+      pergunta8: "5 temporadas"
     }
   
     let acertos = 0;
-    let total = 4;
-  
-    for (let i = 1; i <= total; i++) {
+    let total = 8;
+    
+     // Verifica perguntas 1 a 4 (radio)
+    for (let i = 1; i <= 4; i++) {
       const pergunta = document.querySelector(`input[name="pergunta${i}"]:checked`);
       if (pergunta && pergunta.value === respostasCorretas[`pergunta${i}`]) {
         acertos++;
       }
     }
-  
-    document.getElementById("resultado").innerText = `Você acertou ${acertos} de ${total} perguntas.`;
-  }
+    // Verifica pergunta 7 (date)
+    const dataSelecionada = document.getElementById("p7").value;
+    if (dataSelecionada === respostasCorretas.pergunta7) {
+      acertos++;
+    }
+
+    // Verifica pergunta 5 (checkbox)
+    const respostasCheckbox = ["Lapis Lazuli", "Jasper"];
+    const checkboxesMarcados = Array.from(document.querySelectorAll('input[name="pergunta5"]:checked'))
+                                    .map(checkbox => checkbox.value);
+
+    const todasCorretas = checkboxesMarcados.length === respostasCheckbox.length &&
+      respostasCheckbox.every(opcao => checkboxesMarcados.includes(opcao));
+
+    if (todasCorretas) {
+      acertos++;
+    }
+
+    // Verifica pergunta 6 (select)
+    const valorSelecionado = document.getElementById("p6").value;
+    if (valorSelecionado === respostasCorretas.pergunta6) {
+      acertos++;
+    }
+
+    // Verifica pergunta 6 (select)
+    const valorSelecionado2 = document.getElementById("p8").value;
+    if (valorSelecionado2 === respostasCorretas.pergunta8) {
+      acertos++;
+    }
+
+    // Exibe resultado final
+    document.getElementById("resultado").innerText =
+      `Você acertou ${acertos} de ${total} perguntas.`;
+}
